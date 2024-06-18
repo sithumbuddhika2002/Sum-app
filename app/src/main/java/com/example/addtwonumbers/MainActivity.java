2cd7b1511ce8e6aeb5b84c40ae5801331186e60c
sithumbuddhika2002 <sithumbuddhika2002@gmail.com>
2024-06-19 01:22:27 +0530

    3rd commit

---
+++ app/src/main/java/com/example/addtwonumbers/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.addtwonumbers;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text1 = (EditText) findViewById(R.id.firstNum);
+                EditText text2 = (EditText) findViewById(R.id.secondNum);
+
+                TextView res = (TextView) findViewById(R.id.display);
+
+                int sum = Integer.parseInt(text1.getText().toString())+ Integer.parseInt(text2.getText().toString());
+
+                res.setText(sum + "");
+            }
+        });
+
+    }
+}
